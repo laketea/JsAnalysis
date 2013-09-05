069a0c2d10926fb9bd9fc0d36a402a95e4ad3f7d
zhangwei <laketea@163.com>
2013-09-05 19:41:16 +0800

    rm

--- build/classes/jsanalysis/test/Editor.rs
+++
@@ -1,7 +0,0 @@
-jsanalysis.test.Editor$1
-jsanalysis.test.Editor$2
-jsanalysis.test.KeyWord
-jsanalysis.test.MyEditorView
-jsanalysis.test.MyEditorKit
-jsanalysis.test.MyViewFactory
-jsanalysis.test.Editor
--- build/classes/jsanalysis/ui/ContentPnl.rs
+++
@@ -1,1 +0,0 @@
-jsanalysis.ContentPnl
